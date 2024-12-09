e5937c5c4cda1340dfe784031ef88c21187f2cc2
Farhan Ishraq Omi <omifarhan@gmail.com>
2024-12-09 20:35:04 +0600

    auth and login complete

--- PathaoFarmer/auth_app/serializers.py
+++ PathaoFarmer/auth_app/serializers.py
@@ -4,12 +4,25 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'email')
+        fields = ('username', 'password', 'password2', 'email')
+
+    
+    def validate_password(self, value):
+        if value != self.initial_data['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with that email already exists.")
+        return value
 
     def create(self, validated_data):
+        validated_data.pop('password2')
         user = User.objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
