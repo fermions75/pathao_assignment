88af846e37079f375c17dd8c6f2b22fdadf0b423
Farhan Ishraq Omi <omifarhan@gmail.com>
2024-12-10 22:01:20 +0600

    fix transaction url

--- PathaoFarmer/transactions/urls.py
+++ PathaoFarmer/transactions/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PurchaseLivestock, TransactionHistory
 
 urlpatterns = [
-    path('all', TransactionHistory.as_view(), name='transaction-history'),
     path('purchase/', PurchaseLivestock.as_view(), name='purchase-livestock'),
+    path('', TransactionHistory.as_view(), name='transaction-history'),
 ]
